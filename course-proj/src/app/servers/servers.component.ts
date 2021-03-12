e7c508b5acadc9c6d98551d49161ad1904e98864
Matias Miranda <mmiranda@altimetrik.com>
2021-03-12 20:42:31 -0300

    Lecture 22

--- course-proj/src/app/servers/servers.component.ts
+++ course-proj/src/app/servers/servers.component.ts
@@ -3,7 +3,12 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-servers',
   template: '<app-server></app-server><app-server></app-server>',
-  styleUrls: ['./servers.component.css']
+  //styleUrls: ['./servers.component.css']
+  styles: [`
+    h3 {
+      color: dodgerblue;
+    }
+  `]
 })
 export class ServersComponent implements OnInit {
 
