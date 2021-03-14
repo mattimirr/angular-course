c62ddfb72560354c457f27f819449d32cf62d24d
Matias Miranda <mmiranda@altimetrik.com>
2021-03-14 20:42:29 -0300

    Lecture 26

--- course-proj/src/app/server/server.component.ts
+++ course-proj/src/app/server/server.component.ts
@@ -2,6 +2,14 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: 'app-server',
-    templateUrl:'./server.component.html'
+    templateUrl: './server.component.html'
 })
-export class ServerComponent {}
+export class ServerComponent {
+    serverId = 10;
+    serverStatus = "offline";
+
+    getServerStatus() {
+        return this.serverStatus;
+    }
+
+}
