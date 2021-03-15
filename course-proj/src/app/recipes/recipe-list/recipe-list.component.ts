85af8e392378f300084169a0039885e95cca2ffe
Matias Miranda <mmiranda@altimetrik.com>
2021-03-15 14:25:34 -0300

    Lecture 53 - Created a display for a recipe

--- course-proj/src/app/recipes/recipe-list/recipe-list.component.ts
+++ course-proj/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('Burger', 'A burger from Joshua', 'https://pbs.twimg.com/media/EA-43Z0XYAAGEgz.jpg')
+  ];
 
   constructor() { }
 
