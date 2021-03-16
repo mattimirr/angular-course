d2bf62e10b964057b7be45e6bf345c7ae9b7e8ce
Matias Miranda <mmiranda@altimetrik.com>
2021-03-16 18:16:38 -0300

    Lecture 98 - added HostBinding decorator to bind the background color property of the element and act over it

--- course-proj/src/app/better-highlight/better-highlight.directive.ts
+++ course-proj/src/app/better-highlight/better-highlight.directive.ts
@@ -1,20 +1,23 @@
-import { Directive, ElementRef, HostListener, OnInit, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, HostBinding, HostListener, OnInit, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
 })
 export class BetterHighlightDirective implements OnInit {
+  
+  @HostBinding('style.backgroundColor') backgroundColor: string = 'transparent'; 
+  
   constructor(private elementref: ElementRef, private renderer: Renderer2) { }
   
   ngOnInit() {
     
   }
   @HostListener('mouseenter') mouseHover(eventData : Event) {
-    this.renderer.setStyle(this.elementref.nativeElement,'background-color', 'blue');
+    this.backgroundColor = 'blue';
   }
   
   @HostListener('mouseleave') mouseleave(eventData : Event) {
-    this.renderer.setStyle(this.elementref.nativeElement,'background-color', 'transparent');
+    this.backgroundColor = 'transparent';
   }
-  
+
 }
