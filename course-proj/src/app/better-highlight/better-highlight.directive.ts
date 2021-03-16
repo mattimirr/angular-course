273467890ebc15dd594fe2c6ceb59e5e31dacfb0
Matias Miranda <mmiranda@altimetrik.com>
2021-03-16 18:10:22 -0300

    Lecture 97 - showed HostListener to listen to a event and act on the element, changing the color of the background

--- course-proj/src/app/better-highlight/better-highlight.directive.ts
+++ course-proj/src/app/better-highlight/better-highlight.directive.ts
@@ -1,14 +1,20 @@
-import { Directive, ElementRef, OnInit, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, HostListener, OnInit, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
 })
 export class BetterHighlightDirective implements OnInit {
-
   constructor(private elementref: ElementRef, private renderer: Renderer2) { }
-
+  
   ngOnInit() {
+    
+  }
+  @HostListener('mouseenter') mouseHover(eventData : Event) {
     this.renderer.setStyle(this.elementref.nativeElement,'background-color', 'blue');
   }
-
+  
+  @HostListener('mouseleave') mouseleave(eventData : Event) {
+    this.renderer.setStyle(this.elementref.nativeElement,'background-color', 'transparent');
+  }
+  
 }
