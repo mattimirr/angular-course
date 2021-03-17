e171dbc7a4b97e67ae6b167e4d74febb19575f7b
Matias Miranda <mmiranda@altimetrik.com>
2021-03-17 09:52:51 -0300

    Lecture 112 - We injected the logging service to AccountService and called the log method inside the addacount and update status methods

--- course-proj/src/app/account/account.component.ts
+++ course-proj/src/app/account/account.component.ts
@@ -6,18 +6,16 @@ import { LoggingService } from '../logging.service';
   selector: 'app-account',
   templateUrl: './account.component.html',
   styleUrls: ['./account.component.css'],
-  providers: [LoggingService, AccountService]
 })
 export class AccountComponent {
   @Input() account: { name: string, status: string };
   @Input() id: number;
 
 
-  constructor(private logService: LoggingService, private accountService: AccountService) { }
+  constructor(private accountService: AccountService) { }
 
 
   onSetTo(status: string) {
     this.accountService.updateStatus(this.id, status);
-    this.logService.logStatusChange(status);
   }
 }
--- course-proj/src/app/new-account/new-account.component.ts
+++ course-proj/src/app/new-account/new-account.component.ts
@@ -6,14 +6,12 @@ import { LoggingService } from '../logging.service';
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
   styleUrls: ['./new-account.component.css'],
-  providers: [LoggingService, AccountService]
 })
 export class NewAccountComponent {
 
-  constructor(private logService: LoggingService, private accountService: AccountService) { }
+  constructor( private accountService: AccountService) { }
 
   onCreateAccount(accountName: string, accountStatus: string) {
     this.accountService.addAccount(accountName, accountStatus);
-    this.logService.logStatusChange(accountStatus);
   }
 }
