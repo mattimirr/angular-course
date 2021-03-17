7eec187a0730b1dc5b59518a618476e906850c41
Matias Miranda <mmiranda@altimetrik.com>
2021-03-17 12:28:43 -0300

    Lecture 122 - Added an array of ingredients to Recipe, and now the ingredients are showed in the Recipe Detail Component

--- course-proj/src/app/recipes/recipe.service.ts
+++ course-proj/src/app/recipes/recipe.service.ts
@@ -1,14 +1,18 @@
 
 
 import { EventEmitter } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
 import { Recipe } from "./recipe.model";
 
 export class RecipeService {
-    recipeSelected = new EventEmitter<Recipe>();
-    
-    private recipes: Recipe[] = [
-        new Recipe('Chicken Burger', 'A chicken burger made by Joshua', 'https://pbs.twimg.com/media/EA-43Z0XYAAGEgz.jpg'),
-        new Recipe('Ramen Burger', 'Ramen Burger made by George Motz ', 'https://images.firstwefeast.com/images/c_fill,f_auto,fl_lossy,h_416,q_auto,w_740/n5ick1hfersraz7s4rsl/ramen-burger')
+  recipeSelected = new EventEmitter<Recipe>();
+
+  private recipes: Recipe[] = [
+    new Recipe('Chicken Burger', 'A chicken burger made by Joshua', 'https://pbs.twimg.com/media/EA-43Z0XYAAGEgz.jpg',
+      [new Ingredient('Chicken', 1), new Ingredient('Buns', 1)]),
+    new Recipe('Ramen Burger', 'Ramen Burger made by George Motz ',
+      'https://images.firstwefeast.com/images/c_fill,f_auto,fl_lossy,h_416,q_auto,w_740/n5ick1hfersraz7s4rsl/ramen-burger',
+      [new Ingredient('Ramen Bun', 1), new Ingredient('Miced Meat in kilograms', 500)])
   ];
 
 
