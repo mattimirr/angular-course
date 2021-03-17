ae33a0ced50b0d9a26c2faa3afba1c2be6c84720
Matias Miranda <mmiranda@altimetrik.com>
2021-03-17 12:02:46 -0300

    Lecture 119 - deleted the chain of events to inform the selected recipe and created an event emitter in the recipe service to subscribe to it and get the selected recipe

--- course-proj/src/app/recipes/recipe-list/recipe-list.component.ts
+++ course-proj/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component,  OnInit} from '@angular/core';
+
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
 
@@ -8,7 +9,6 @@ import { RecipeService } from '../recipe.service';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes : Recipe[];
 
   constructor(private recipeService : RecipeService) { }
@@ -17,8 +17,5 @@ export class RecipeListComponent implements OnInit {
     this.recipes = this.recipeService.getRecipes();
   }
 
-  onSelectedRecipe(recipe: Recipe) {
-    this.recipeWasSelected.emit(recipe);
-  }
 
 }
--- course-proj/src/app/recipes/recipe.service.ts
+++ course-proj/src/app/recipes/recipe.service.ts
@@ -1,14 +1,17 @@
 
 
+import { EventEmitter } from "@angular/core";
 import { Recipe } from "./recipe.model";
 
 export class RecipeService {
+    recipeSelected = new EventEmitter<Recipe>();
+    
     private recipes: Recipe[] = [
         new Recipe('Chicken Burger', 'A chicken burger made by Joshua', 'https://pbs.twimg.com/media/EA-43Z0XYAAGEgz.jpg'),
         new Recipe('Ramen Burger', 'Ramen Burger made by George Motz ', 'https://images.firstwefeast.com/images/c_fill,f_auto,fl_lossy,h_416,q_auto,w_740/n5ick1hfersraz7s4rsl/ramen-burger')
   ];
 
-  
+
   getRecipes() {
     return this.recipes.slice();
   }
