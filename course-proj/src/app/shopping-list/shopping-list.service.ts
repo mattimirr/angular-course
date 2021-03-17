3fd57d5b9e5370e043748b60ccd622250f4f8ba0
Matias Miranda <mmiranda@altimetrik.com>
2021-03-17 12:21:24 -0300

    Lecture 121 - Fixed the ingredient list with an event emitter so when someone add an ingredient it will be reflected in the list of the service

--- course-proj/src/app/shopping-list/shopping-list.service.ts
+++ course-proj/src/app/shopping-list/shopping-list.service.ts
@@ -1,6 +1,8 @@
+import { EventEmitter } from "@angular/core";
 import { Ingredient } from "../shared/ingredient.model";
 
 export class ShoppingListService {
+    ingredientsChanged = new EventEmitter<Ingredient[]>();
     private ingredients: Ingredient[] = [
         new Ingredient('Buns', 2), new Ingredient('Chicken', 1), new Ingredient('Ramen buns', 1),
         new Ingredient('Minced Meat in kilograms', 1)
@@ -12,5 +14,6 @@ export class ShoppingListService {
 
     addIngredient(ingredient: Ingredient) {
         this.ingredients.push(ingredient);
+        this.ingredientsChanged.emit(this.ingredients.slice());
     }
 }
