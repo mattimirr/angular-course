0c0aced06a78d0d10c160b5a7c7e7f6a0c25c851
Matias Miranda <mmiranda@altimetrik.com>
2021-03-18 16:18:21 -0300

    Lecture 173 - Created an observable manually with new Observable and logged like in lectura 172

--- course-proj/src/app/home/home.component.ts
+++ course-proj/src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { interval, Subscription } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -7,15 +7,25 @@ import { interval, Subscription } from 'rxjs';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit, OnDestroy {
-  private firstSub : Subscription;
+  private firstSub: Subscription;
   constructor() { }
 
   ngOnInit() {
-    this.firstSub = interval(1000).subscribe(count => {
-      console.log(count);
+    // this.firstSub = interval(1000).subscribe(count => {
+    //   console.log(count);
+    // });
+    const customIntervalObservable = new Observable((observer) => {
+      let count = 0;
+      setInterval(() => {
+        observer.next(count);
+        count++;
+      }, 1000)
     });
+    this.firstSub = customIntervalObservable.subscribe(data => {
+      console.log(data);
+    })
   }
-  ngOnDestroy(){
+  ngOnDestroy() {
     this.firstSub.unsubscribe();
   }
 
