e7c3f863d45f17adf12e1a46c94643fe6470d9fa
Matias Miranda <mmiranda@altimetrik.com>
2021-03-24 14:04:07 -0300

    Lecture 199 - final of TD

--- course-proj/src/app/app.component.ts
+++ course-proj/src/app/app.component.ts
@@ -11,7 +11,15 @@ export class AppComponent {
 
   defaultQuestion = 'pet';
   answer = '';
-  genders = ['female', 'male','rather not say'];
+  genders = ['female', 'male', 'rather not say'];
+  user = {
+    username: '',
+    email: '',
+    secretQuestion: '',
+    answer: '',
+    gender: ''
+  }
+  subbmited = false;
 
   suggestUserName() {
     const suggestedName = 'Superuser';
@@ -35,6 +43,13 @@ export class AppComponent {
 
   onSubmit() {
     console.log(this.signUpForm);
+    this.user.username = this.signUpForm.value.userData.username;
+    this.user.email = this.signUpForm.value.userData.email;
+    this.user.secretQuestion = this.signUpForm.value.secret;
+    this.user.answer = this.signUpForm.value.questionAnswer;
+    this.user.gender = this.signUpForm.value.gender;
+    this.subbmited = true;
+
   }
 
 }
