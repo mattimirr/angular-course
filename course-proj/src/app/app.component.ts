cca58ff95e5e62002489f50266cb9e0a5ccb658c
Matias Miranda <mmiranda@altimetrik.com>
2021-03-24 16:48:57 -0300

    Base Reactive form - only created our FormGroup with the FormControls

--- course-proj/src/app/app.component.ts
+++ course-proj/src/app/app.component.ts
@@ -1,55 +1,20 @@
-import { Component, ViewChild } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormControl, FormGroup, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  @ViewChild('form') signUpForm: NgForm;
-
-  defaultQuestion = 'pet';
-  answer = '';
+export class AppComponent implements OnInit {
   genders = ['female', 'male', 'rather not say'];
-  user = {
-    username: '',
-    email: '',
-    secretQuestion: '',
-    answer: '',
-    gender: ''
-  }
-  subbmited = false;
+  signupForm: FormGroup;
 
-  suggestUserName() {
-    const suggestedName = 'Superuser';
-    // this.signUpForm.setValue({
-    //   userData: {
-    //     username: suggestedName,
-    //     email: ''
-    //   },
-    //   secret: 'pet',
-    //   questionAnswer: '',
-    //   gender:'male'
-    // });
-    this.signUpForm.form.patchValue(
-      {
-        userData:
-        {
-          username: suggestedName
-        }
-      });
+  ngOnInit() {
+    this.signupForm = new FormGroup({
+      'username': new FormControl(null),
+      'email': new FormControl(null),
+      'gender': new FormControl('male')
+    });
   }
-
-  onSubmit() {
-    console.log(this.signUpForm);
-    this.user.username = this.signUpForm.value.userData.username;
-    this.user.email = this.signUpForm.value.userData.email;
-    this.user.secretQuestion = this.signUpForm.value.secret;
-    this.user.answer = this.signUpForm.value.questionAnswer;
-    this.user.gender = this.signUpForm.value.gender;
-    this.subbmited = true;
-    this.signUpForm.reset();
-  }
-
 }
