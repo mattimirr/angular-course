f55a9c627fe96a5eec2474a216ba98c8638b15e6
Matias Miranda <mmiranda@altimetrik.com>
2021-03-24 16:33:51 -0300

    Assignment complete

--- course-proj/src/app/app.component.ts
+++ course-proj/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  
+  @ViewChild('form', { static: false }) signupForm: NgForm;
+  defaultSubscription = 'advanced';
 
+
+  onSubmit() { 
+    console.log(this.signupForm);
+
+  }
 }
