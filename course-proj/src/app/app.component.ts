51e87b1e4417ed02d2c9f5efbcae726cb2c3b791
Matias Miranda <mmiranda@altimetrik.com>
2021-03-24 18:29:30 -0300

    Lecture 209 210 - Added an FormArray and go through it with ngFor to show and add a new input to the form

--- course-proj/src/app/app.component.ts
+++ course-proj/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
+import { fromEventPattern } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -16,12 +17,21 @@ export class AppComponent implements OnInit {
         'username': new FormControl(null, Validators.required),
         'email': new FormControl(null, [Validators.required, Validators.email]),
       }),
-      'gender': new FormControl('male')
+      'gender': new FormControl('male'),
+      'hobbies': new FormArray([])
     });
   }
 
   onSubmit() {
     console.log(this.signupForm);
+  }
 
+  onAddHobby() {
+    const formControl = new FormControl(null, Validators.required);
+    (<FormArray>this.signupForm.get('hobbies')).push(formControl);
+  }
+
+  get controls() {
+    return (this.signupForm.get('hobbies') as FormArray).controls;
   }
 }
