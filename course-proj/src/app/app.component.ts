e2bc382184d604ceade83404ea25aae72bcac9e1
Matias Miranda <mmiranda@altimetrik.com>
2021-03-24 14:11:58 -0300

    Assignment 6 initial state

--- course-proj/src/app/app.component.ts
+++ course-proj/src/app/app.component.ts
@@ -1,21 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-
-import { CounterService } from './counter.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  inToAct: number = 0;
-  actToIn: number = 0;
+export class AppComponent {
 
-  constructor(private counterService: CounterService) {
-  }
-
-  ngOnInit() {
-    this.counterService.activeUsers.subscribe((active: number) => {this.inToAct = active});
-    this.counterService.inactiveUsers.subscribe((inactive: number) => {this.actToIn = inactive});
-  }
 }
