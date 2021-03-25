63aea831de081a35816303fdeab97a55adcd852f
Matias Miranda <mmiranda@altimetrik.com>
2021-03-25 14:10:19 -0300

    Assignment 7 complete

--- course-proj/src/app/app.component.ts
+++ course-proj/src/app/app.component.ts
@@ -1,19 +1,39 @@
-import { Component, ViewChild } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  
-  @ViewChild('form', { static: false }) signupForm: NgForm;
-  defaultSubscription = 'advanced';
+export class AppComponent implements OnInit {
+  projectStatus = ['Stable', 'Critical', 'Finished'];
+  projectForm: FormGroup;
 
 
-  onSubmit() { 
-    console.log(this.signupForm);
+  ngOnInit() {
+    this.projectForm = new FormGroup({
+      'projectName': new FormControl(null, [Validators.required, null, this.forbiddenPName.bind(this)]),
+      'email': new FormControl(null, [Validators.required, Validators.email]),
+      'projectStatus': new FormControl('Stable')
+    });
+  }
 
+  onSubmit() {
+    console.log(this.projectForm);
+  }
+
+  forbiddenPName(control: FormControl): Promise<any> | Observable<any> {
+    const promise = new Promise<any>((resolve, reject) => {
+      setTimeout(() => {
+        if (control.value === 'Test') {
+          resolve({'projectNameIsForbidden': true})
+        } else {
+          resolve(null);
+        }
+      }, 1500);
+    });
+    return promise;
   }
 }
