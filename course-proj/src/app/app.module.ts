7aac54bd03ee5dd40b89d3bcfcc10546177f2457
Matias Miranda <mmiranda@altimetrik.com>
2021-03-26 10:30:14 -0300

    Lecture 255 Sended a new Post request to firebase and subscribed to it

--- course-proj/src/app/app.module.ts
+++ course-proj/src/app/app.module.ts
@@ -1,23 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { ReversePipe } from './reverse.pipe';
-import { SortPipe } from './sort.pipe';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    SortPipe,
-    ReversePipe
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-  ],
-  providers: [SortPipe, ReversePipe],
+  declarations: [AppComponent],
+  imports: [BrowserModule, FormsModule, HttpClientModule],
+  providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
