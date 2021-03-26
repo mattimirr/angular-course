f790b529fa12d1c40b07752c8a3721ebb26df237
Matias Miranda <mmiranda@altimetrik.com>
2021-03-26 10:47:34 -0300

    Lecture 258 - we added the type of the get and post request to know what type of object we are dealing with

--- course-proj/src/app/app.component.ts
+++ course-proj/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
+import { Post } from './post.model';
 
 @Component({
   selector: 'app-root',
@@ -16,9 +17,9 @@ export class AppComponent implements OnInit {
     this.fetchPosts();
   }
 
-  onCreatePost(postData: { title: string; content: string }) {
+  onCreatePost(postData: Post) {
     // Send Http request
-    this.http.post(
+    this.http.post<{ name: string }>(
       'https://ng-angular-project-7a530-default-rtdb.firebaseio.com/posts.json',
       postData
     ).subscribe((responseData => {
@@ -35,19 +36,19 @@ export class AppComponent implements OnInit {
     // Send Http request
   }
   private fetchPosts() {
-    this.http.get(
+    this.http.get<{ [key: string]: Post }>(
       'https://ng-angular-project-7a530-default-rtdb.firebaseio.com/posts.json')
       .pipe(map(responseData => {
-        const postsArray = [];
+        const postsArray: Post[] = [];
         for (const key in responseData) {
-          if(responseData.hasOwnProperty(key)){
+          if (responseData.hasOwnProperty(key)) {
             postsArray.push({ ...responseData[key], id: key });
           }
         }
         return postsArray;
       }))
-      .subscribe(posts => {
-        console.log(posts);
-      });
+        .subscribe(posts => {
+          console.log(posts);
+        });
   }
 }
