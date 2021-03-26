b88144e920bc2ce1512c0693a24ae0ca7d2db4f5
Matias Miranda <mmiranda@altimetrik.com>
2021-03-26 19:56:56 -0300

    lecture 271- 275 - Added interceptor before sending the request and an interceptor with pipe and tap to the response received

---
+++ course-proj/src/app/auth-interceptor.service.ts
@@ -0,0 +1,18 @@
+import { HttpEventType, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
+import { tap } from "rxjs/operators";
+
+export class AuthInterceptorService implements HttpInterceptor {
+    intercept(request: HttpRequest<any>, next: HttpHandler) {
+        console.log('Request is on its way');
+        console.log(request.url);
+        const modifiedRequest = request.clone({
+            headers: request.headers.append('Auth', 'abc');
+        });
+        return next.handle(modifiedRequest).pipe(tap(event => {
+            if (event.type === HttpEventType.Response) {
+                console.log('Response arrived body data: ');
+                console.log(event.body);
+            }
+        }));
+    }
+}
