0cf35b8b1614f120911ff4ea36c1716e546c0343
Matias Miranda <mmiranda@altimetrik.com>
2021-03-26 16:37:53 -0300

    Lecture 270 - observing and managing events of the response

--- course-proj/src/app/posts.service.ts
+++ course-proj/src/app/posts.service.ts
@@ -1,7 +1,7 @@
-import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { HttpClient, HttpEventType, HttpHeaders, HttpParams } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Subject, throwError } from "rxjs";
-import { map, catchError } from "rxjs/operators";
+import { map, catchError, tap } from "rxjs/operators";
 
 import { Post } from "./post.model";
 
@@ -17,7 +17,10 @@ export class PostsService {
 
         this.http.post<{ name: string }>(
             'https://ng-angular-project-7a530-default-rtdb.firebaseio.com/posts.json',
-            postData
+            postData,
+            {
+                observe: 'response'
+            }
         ).subscribe(responseData => {
             console.log(responseData);
         }, error => {
@@ -29,7 +32,8 @@ export class PostsService {
         return this.http.get<{ [key: string]: Post }>(
             'https://ng-angular-project-7a530-default-rtdb.firebaseio.com/posts.json',
             {
-                headers: new HttpHeaders({"Custom-header": 'hello'})
+                headers: new HttpHeaders({ "Custom-header": 'hello' }),
+                params: new HttpParams().set('print', 'pretty')
             })
             .pipe(
                 map(responseData => {
@@ -48,6 +52,14 @@ export class PostsService {
     }
 
     deletePosts() {
-        return this.http.delete('https://ng-angular-project-7a530-default-rtdb.firebaseio.com/posts.json');
+        return this.http.delete('https://ng-angular-project-7a530-default-rtdb.firebaseio.com/posts.json',
+            {
+                observe: 'events'
+            }).pipe(tap(event => { 
+                console.log(event);
+                if(event.type === HttpEventType.Response){
+                    console.log(event.body)
+                }
+            }));
     }
 }
