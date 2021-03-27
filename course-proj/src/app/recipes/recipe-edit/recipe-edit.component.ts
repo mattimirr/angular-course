13937116c767ceb404c78f3445bdb24d304e35bf
Matias Miranda <mmiranda@altimetrik.com>
2021-03-27 13:37:44 -0300

    Lecture to 229 - added form names and method to init the form in case of a new recipe or editing a created one

--- course-proj/src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ course-proj/src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Params } from '@angular/router';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-edit',
@@ -8,15 +10,39 @@ import { ActivatedRoute, Params } from '@angular/router';
 })
 export class RecipeEditComponent implements OnInit {
   id: number;
-  editMode= false;
-  constructor(private route: ActivatedRoute) { }
+  editMode = false;
+  recipeForm: FormGroup;
+  constructor(private route: ActivatedRoute, private recipeService: RecipeService) { }
 
   ngOnInit() {
     this.route.params
       .subscribe((params: Params) => {
         this.id = +params['id'];
         this.editMode = params['id'] != null;
+        this.initForm();
       });
+  }
+  
+  onSubmit() {
+
+  }
+
+  private initForm() {
+    let recipeImagePath = '';
+    let recipeDescription = '';
+    let recipeName = '';
+
+    if (this.editMode) {
+      const recipe = this.recipeService.getRecipe(this.id);
+      recipeName = recipe.name;
+      recipeImagePath = recipe.imagePath;
+      recipeDescription = recipe.description;
+    }
+    this.recipeForm = new FormGroup({
+      'name': new FormControl(recipeName, Validators.required),
+      'imagePath': new FormControl(recipeImagePath, Validators.required),
+      'description': new FormControl(recipeDescription, Validators.required)
+    });
   }
 
 }
