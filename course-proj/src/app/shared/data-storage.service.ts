9b03f8f1065ceca4778d994d320101c5b18288f7
Matias Miranda <mmiranda@altimetrik.com>
2021-03-29 10:41:27 -0300

    Completed Section Http

--- course-proj/src/app/shared/data-storage.service.ts
+++ course-proj/src/app/shared/data-storage.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
-
+import { map, tap } from 'rxjs/operators';
 import { RecipeService } from "../recipes/recipe.service";
 import { Recipe } from "../recipes/recipe.model";
 
@@ -15,9 +15,15 @@ export class DataStorageService {
     }
 
     fetchRecipes() {
-        this.httpClient.get<Recipe[]>('https://ng-course-recipebook-c8560-default-rtdb.firebaseio.com/recipes.json')
-            .subscribe(response => {
-                this.recipeService.setRecipes(response);
-            });
+       return this.httpClient.get<Recipe[]>('https://ng-course-recipebook-c8560-default-rtdb.firebaseio.com/recipes.json')
+            .pipe(
+                map((recipes) => {
+                    return recipes.map(recipe => {
+                        return { ...recipe, ingredients: recipe.ingredients ? recipe.ingredients : [] };
+                    });
+                }),
+                tap(recipes => {
+                    this.recipeService.setRecipes(recipes);
+                }));
     }
 }
