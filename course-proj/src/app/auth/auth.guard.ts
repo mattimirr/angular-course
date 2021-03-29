18dbf9da07ed7abd7a664b0863b75cc6919143f7
Matias Miranda <mmiranda@altimetrik.com>
2021-03-29 20:19:10 -0300

    Lecture 306 - In the auth guard, when we call the BehaviourSubject we use a pipe, and inside it a map to return true of canActivate or to auth page if false, but we call this pipe every time is changed the User, as a Subject, this is a shared observable, so we use take(1) to hear the subject only one time and die

--- course-proj/src/app/auth/auth.guard.ts
+++ course-proj/src/app/auth/auth.guard.ts
@@ -1,9 +1,8 @@
 import { Injectable } from "@angular/core";
 import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from "@angular/router";
-import { userInfo } from "node:os";
 import { Observable } from "rxjs";
-import { map } from "rxjs/operators";
-import { ShoppingEditComponent } from "../shopping-list/shopping-edit/shopping-edit.component";
+import { map, take } from "rxjs/operators";
+
 import { AuthService } from "./auth.service";
 
 @Injectable({ providedIn: 'root' })
@@ -14,14 +13,15 @@ export class AuthGuard implements CanActivate {
     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot)
         : boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
         return this.authService.user
-            .pipe
-            (map(user => {
-                const isAuth = !!user;
-                if (isAuth) {
-                    return true;
-                }
-                return this.router.createUrlTree(['./auth']);
-            }));
+            .pipe(
+                take(1),
+                map(user => {
+                    const isAuth = !!user;
+                    if (isAuth) {
+                        return true;
+                    }
+                    return this.router.createUrlTree(['./auth']);
+                }));
     }
 
 }
