cd0427014b401da16ca08ca6e99123ff9428b65b
Matias Miranda <mmiranda@altimetrik.com>
2021-03-30 11:40:12 -0300

    Finished dynamic components, added a dynamic component with reference and factory resolver

--- course-proj/src/app/auth/auth.component.ts
+++ course-proj/src/app/auth/auth.component.ts
@@ -1,7 +1,10 @@
-import { Component, OnDestroy } from "@angular/core";
-import { Form, NgForm } from "@angular/forms";
+import { Component, ComponentFactoryResolver, OnDestroy, ViewChild } from "@angular/core";
+import { NgForm } from "@angular/forms";
 import { Router } from "@angular/router";
 import { Observable, Subscription } from "rxjs";
+
+import { AlertComponent } from "../shared/alert/alert.component";
+import { PlaceholderDirective } from "../shared/placeholder.directive";
 import { AuthResponseData, AuthService } from "./auth.service";
 
 @Component({
@@ -9,12 +12,16 @@ import { AuthResponseData, AuthService } from "./auth.service";
     templateUrl: './auth.component.html',
     styleUrls: ['./auth.component.css']
 })
-export class AuthComponent {
+export class AuthComponent implements OnDestroy{
+    @ViewChild(PlaceholderDirective) alertHost: PlaceholderDirective;
     isLoginMode = true;
     isLoading = false;
     error: string = null;
+    private closeSub: Subscription;
 
-    constructor(private authService: AuthService, private router: Router) { }
+    constructor(private authService: AuthService,
+        private router: Router,
+        private componentFactoryResolver: ComponentFactoryResolver) { }
 
     onChangeMode() {
         this.isLoginMode = !this.isLoginMode;
@@ -40,6 +47,8 @@ export class AuthComponent {
             this.router.navigate(['./recipes']);
         }, errorMessage => {
             this.error = errorMessage;
+            console.log(errorMessage)
+            this.showErrorAlert(errorMessage);
             this.isLoading = false;
         });
 
@@ -47,6 +56,25 @@ export class AuthComponent {
 
     onHandleError() {
         this.error = null;
+    }
+    ngOnDestroy(){
+        if(this.closeSub){
+            this.closeSub.unsubscribe();
+        }
+    }
+
+    private showErrorAlert(message: string) {
+        const alertCmpFactory = this.componentFactoryResolver.resolveComponentFactory(AlertComponent);
+        const hostviewContainerRef = this.alertHost.viewContainerRef;
+        hostviewContainerRef.clear();
+
+        const componentRef = hostviewContainerRef.createComponent(alertCmpFactory);
+
+        componentRef.instance.message = message;
+        this.closeSub = componentRef.instance.close.subscribe(() => {
+            this.closeSub.unsubscribe();
+            hostviewContainerRef.clear();
+        })
     }
 
 }
