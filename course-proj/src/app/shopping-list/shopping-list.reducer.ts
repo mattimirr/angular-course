b110a8cacbfe02ef47ecc79eda2008da61c1765c
Matias Miranda <mmiranda@altimetrik.com>
2021-03-31 11:33:47 -0300

    Lecture 347 - Added a new Reducer, that at the end is only a function that receives two arg, the state, that at the beggining we set it with our initialState, in this case two ingredients ,and a second arg that is the action to perform

---
+++ course-proj/src/app/shopping-list/shopping-list.reducer.ts
@@ -0,0 +1,12 @@
+
+import { Ingredient } from "../shared/ingredient.model";
+
+const initialState = {
+    ingredients: [
+        new Ingredient('Buns', 2), new Ingredient('Chicken', 1), new Ingredient('Ramen buns', 1),
+        new Ingredient('Minced Meat in kilograms', 1)
+    ]
+};
+export function shoppingListReducer(state, action) {
+
+}
