2a61ad551b37b390f53d6980c9a7457d62db0bc4
Matias Miranda <mmiranda@altimetrik.com>
2021-04-01 18:47:20 -0300

    Lecture 363 - Deleted the behaviourSubject and fixed the errors calling the store and auth.user in the cases

--- course-proj/src/app/auth/auth-interceptor.service.ts
+++ course-proj/src/app/auth/auth-interceptor.service.ts
@@ -1,17 +1,21 @@
 import { HttpHandler, HttpInterceptor, HttpParams, HttpRequest } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { exhaustMap, take } from "rxjs/operators";
+import { exhaustMap, map, take } from "rxjs/operators";
+import { Store } from '@ngrx/store';
+import * as fromApp from '../store/app.reducers';
 
 import { AuthService } from "./auth.service";
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
 
-    constructor(private authService: AuthService) { }
+    constructor(private authService: AuthService, private store: Store<fromApp.AppState>) { }
     intercept(request: HttpRequest<any>, next: HttpHandler) {
-        this.authService.user.subscribe()
-        return this.authService.user.pipe(
+        return this.store.select('auth').pipe(
             take(1),
+            map(authState => {
+                return authState.user;
+            }),
             exhaustMap(user => {
 
                 if (!user) {
--- course-proj/src/app/auth/auth.guard.ts
+++ course-proj/src/app/auth/auth.guard.ts
@@ -3,18 +3,21 @@ import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTr
 import { Observable } from "rxjs";
 import { map, take } from "rxjs/operators";
 
+import { Store } from '@ngrx/store';
+import * as fromApp from '../store/app.reducers';
 import { AuthService } from "./auth.service";
 
 @Injectable({ providedIn: 'root' })
 export class AuthGuard implements CanActivate {
 
-    constructor(private authService: AuthService, private router: Router) { }
+    constructor(private authService: AuthService, private router: Router, private store: Store<fromApp.AppState>) { }
 
     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot)
         : boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
-        return this.authService.user
+        return this.store.select('auth')
             .pipe(
                 take(1),
+                map(authState => { return authState.user; }),
                 map(user => {
                     const isAuth = !!user;
                     if (isAuth) {
