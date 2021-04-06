c50a01c3fe65d66580706096da36943a072d6a5c
Matias Miranda <mmiranda@altimetrik.com>
2021-04-06 11:51:24 -0300

    Lecture 408 - created a new animation, it has two states, normal and highlighted, in the first one, the background color is red and is positioned at x = 0, in highlighted the color is blue and is positioned 100px off the X. Finally we create a transition from one to another and animate it with time, that is the basic animation

--- course-proj/src/app/app.component.ts
+++ course-proj/src/app/app.component.ts
@@ -1,13 +1,33 @@
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
+  animations: [
+    trigger('divState', [
+      state('normal', style({
+        backgroundColor: 'red',
+        transform: 'translateX(0)'
+      })),
+      state('highlighted', style({
+        backgroundColor: 'blue',
+        transform: 'translateX(100px)'
+      })),
+      transition('normal => highlighted', animate(300)),
+      transition('highlighted => normal', animate(400))
+    ])
+  ]
 })
 export class AppComponent {
+  state = 'normal';
   list = ['Milk', 'Sugar', 'Bread'];
 
-    onAdd(item) {
-      this.list.push(item);
-    }
+  onAdd(item) {
+    this.list.push(item);
+  }
+
+  onAnimate() {
+    this.state == 'normal' ? this.state = 'highlighted' : this.state = 'normal';
+  }
 }
