9aba76c78eb716576a1565307b693effc2d50e90
Matias Miranda <mmiranda@altimetrik.com>
2021-04-06 12:34:57 -0300

    Lecture 411 - Added a new animation, when inserting a new element to the list it came from the left and opacity 0, and when we delete one, it goes to the right and to opacity 0

--- course-proj/src/app/app.component.ts
+++ course-proj/src/app/app.component.ts
@@ -14,20 +14,66 @@ import { Component } from '@angular/core';
         backgroundColor: 'blue',
         transform: 'translateX(100px)'
       })),
+      transition('normal <=> highlighted', animate(300)),
+    ]),
+    trigger('wildState', [
+      state('normal', style({
+        backgroundColor: 'red',
+        transform: 'translateX(0) scale(1)'
+      })),
+      state('highlighted', style({
+        backgroundColor: 'blue',
+        transform: 'translateX(100px) scale(1)'
+      })),
+      state('shrink', style({
+        backgroundColor: 'green',
+        transform: 'translateX(0px) scale(0.5)'
+      })),
       transition('normal => highlighted', animate(300)),
-      transition('highlighted => normal', animate(400))
+      transition('highlighted => normal', animate(800)),
+      transition('* <=> shrink', [
+        style({
+          backgroundColor: 'orange'
+        }), animate(1000, style({
+          borderRadius: '50px'
+        })),
+        animate(500)
+      ]),
+    ]),
+    trigger('list1', [
+      state('in', style({
+        opacity: 1,
+        transform: 'translateX(0)'
+      })),
+      transition('void => *', [style({
+        opacity: 0,
+        transform: 'translateX(-100px)'
+      }), animate(300)]),
+      transition('* => void', animate(300, style({
+        transform: 'translateX(100px)',
+        opacity: 0
+      })))
     ])
   ]
 })
 export class AppComponent {
   state = 'normal';
+  wildState = 'normal';
   list = ['Milk', 'Sugar', 'Bread'];
 
   onAdd(item) {
     this.list.push(item);
   }
+  onDelete(index: number){
+    this.list.splice(index, 1);
+  }
 
   onAnimate() {
     this.state == 'normal' ? this.state = 'highlighted' : this.state = 'normal';
+    this.wildState == 'normal' ? this.wildState = 'highlighted' : this.wildState = 'normal';
+  }
+
+  onShrink() {
+    this.wildState = 'shrink';
   }
 }
